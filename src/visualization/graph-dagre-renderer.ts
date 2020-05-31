ffc4877aaadbff93e946adbcf8f51f29238a18ec
abuob <15157089+abuob@users.noreply.github.com>
2020-05-31 17:02:57 +0200

    dagre-graph-rendering: Add aditional focus options

--- src/visualization/graph-dagre-renderer.ts
+++ src/visualization/graph-dagre-renderer.ts
@@ -1,6 +1,6 @@
 import { IYaniceArgs } from '../config/args-parser';
 import { IYaniceCommand, IYaniceConfig } from '../config/config.interface';
-import { IDirectedGraph } from '../directed-graph/directed-graph';
+import { DirectedGraphUtil, IDirectedGraph } from '../directed-graph/directed-graph';
 import { ChangedProjects } from '../git-diff/changed-projects';
 
 interface IYaniceGraphNodeInfo {
@@ -8,6 +8,8 @@ interface IYaniceGraphNodeInfo {
     scope: string;
     parents: string[];
     children: string[];
+    ancestors: string[];
+    descendants: string[];
     responsibles: string[];
     changedFiles: string[];
     isAffected: boolean;
@@ -32,6 +34,8 @@ export class GraphDagreRenderer {
                     scope: yaniceArgs.givenScope,
                     parents: node.getParents().map((e) => e.name),
                     children: node.getChildren().map((e) => e.name),
+                    ancestors: DirectedGraphUtil.getAncestorsAndSelfForSingleNode(depGraph, node.name),
+                    descendants: DirectedGraphUtil.getDescendantsAndSelfForSingleNode(depGraph, node.name),
                     responsibles: projectOrUndefined ? projectOrUndefined.responsibles : [],
                     changedFiles: projectOrUndefined
                         ? changedFiles.filter((filePath) => ChangedProjects.isFilePathPartOfProject(projectOrUndefined, filePath))
