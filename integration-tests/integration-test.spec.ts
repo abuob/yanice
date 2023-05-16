2e028d3a721b0325b970b343e39d8abba11da449
Andreas Buob <15157089+abuob@users.noreply.github.com>
2023-05-16 13:28:37 +0200

    Add integration-test for simple dependency

--- integration-tests/integration-test.spec.ts
+++ integration-tests/integration-test.spec.ts
@@ -8,17 +8,30 @@ describe('yanice', () => {
 
     describe('--all --output-only', () => {
         it('should be able to read its config file and print out all project files for a given scope', () => {
-            const output: string = IntegrationTestUtil.executeYaniceWithArgs('flat-all-projects-have-commands --all --output-only');
+            const output: string = IntegrationTestUtil.executeYaniceWithArgs(
+                'flat-all-projects-have-commands --all --output-only --rev=HEAD'
+            );
             const printedProjects: string[] = IntegrationTestUtil.getNonEmptyLines(output);
             expect(printedProjects).to.have.length(3);
             expect(printedProjects).to.have.same.members(['A', 'B', 'C']);
         });
 
         it('should only print projects command for given scope', () => {
-            const output: string = IntegrationTestUtil.executeYaniceWithArgs('flat-projects-one-command --all --output-only');
+            const output: string = IntegrationTestUtil.executeYaniceWithArgs('flat-projects-one-command --all --output-only --rev=HEAD');
             const printedProjects: string[] = IntegrationTestUtil.getNonEmptyLines(output);
             expect(printedProjects).to.have.length(1);
             expect(printedProjects).to.have.same.members(['A']);
+        });
+    });
+
+    describe('--output-only', () => {
+        it('should print the changed project and all its dependents', () => {
+            IntegrationTestUtil.touchProject('project-B');
+            const output: string = IntegrationTestUtil.executeYaniceWithArgs('a-depends-on-b --output-only --rev=HEAD');
+            const printedProjects: string[] = IntegrationTestUtil.getNonEmptyLines(output);
+            expect(printedProjects).to.have.length(2);
+            expect(printedProjects).to.have.same.members(['A', 'B']);
+            IntegrationTestUtil.resetChanges();
         });
     });
 });
