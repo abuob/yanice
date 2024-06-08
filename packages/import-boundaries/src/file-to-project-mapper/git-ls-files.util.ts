0d1abbaeb0141706779619d4c0e83c5b0bd1173d
Andreas Buob <15157089+abuob@users.noreply.github.com>
2024-06-08 11:59:13 +0200

    import-boundaries-plugin: Ignore files which were deleted from the worktree but not yet from the git-index

--- packages/import-boundaries/src/file-to-project-mapper/git-ls-files.util.ts
+++ packages/import-boundaries/src/file-to-project-mapper/git-ls-files.util.ts
@@ -1,12 +1,20 @@
 import { spawn } from 'node:child_process';
+import fs from 'node:fs';
 import path from 'node:path';
 
 export class GitLsFilesUtil {
     public static async getAllAbsoluteFilePathsInYaniceRoot(gitRepoRootPath: string, yaniceJsonDirectoryPath: string): Promise<string[]> {
         const allFilesInYaniceRootRelativeToGitRoot: string[] = await GitLsFilesUtil.gitLsFiles(gitRepoRootPath, yaniceJsonDirectoryPath);
-        return allFilesInYaniceRootRelativeToGitRoot.map((filePathRelativeToGitRoot: string): string => {
-            return path.join(gitRepoRootPath, filePathRelativeToGitRoot);
-        });
+        return allFilesInYaniceRootRelativeToGitRoot
+            .map((filePathRelativeToGitRoot: string): string => {
+                return path.join(gitRepoRootPath, filePathRelativeToGitRoot);
+            })
+            .filter((absoluteFilePath: string): boolean => {
+                // When a file is deleted but the change is not yet staged, git-ls-files still returns that file.
+                // Could probably be suppressed with the correct incantation of git-flags,
+                // but that is unfortunately infinitely less trivial than this check here...
+                return fs.existsSync(absoluteFilePath);
+            });
     }
 
     public static async gitLsFiles(workingDirectory: string, lsFilesDirectoryParameter: string): Promise<string[]> {
